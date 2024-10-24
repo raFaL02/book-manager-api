74713d6551340fecd2c5a997ebba79b2a975fa81
Rafael de Lima <rafalimabcl@gmail.com>
2024-10-23 23:12:58 -0300

    feat: create books table

---
+++ src/main/resources/db/migrations/V1__create-books-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE books (
+    id UUID PRIMARY KEY,
+    title VARCHAR(250) NOT NULL,
+    isbn VARCHAR(20) NOT NULL,
+    generes VARCHAR(50) NOT NULL ,
+    publishDate TIMESTAMP NOT NULL,
+    authors_id UUID,
+    FOREIGN KEY (authors_id) REFERENCES authors(id) ON DELETE CASCADE
+);
