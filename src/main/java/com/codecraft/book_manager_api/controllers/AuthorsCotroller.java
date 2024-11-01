c3b9f1578117a41854a414d076489bdd7d389842
Rafael de Lima <rafalimabcl@gmail.com>
2024-11-01 00:19:15 -0300

    refactor: refactor authors controller with services

--- src/main/java/com/codecraft/book_manager_api/controllers/AuthorsCotroller.java
+++ src/main/java/com/codecraft/book_manager_api/controllers/AuthorsCotroller.java
@@ -2,11 +2,12 @@ package com.codecraft.book_manager_api.controllers;
 
 import com.codecraft.book_manager_api.domain.author.Author;
 import com.codecraft.book_manager_api.domain.book.Book;
-import com.codecraft.book_manager_api.repositories.AuthorsRepository;
+import com.codecraft.book_manager_api.services.AuthorsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @RestController
@@ -14,39 +15,35 @@ import java.util.UUID;
 public class AuthorsCotroller {
 
     @Autowired
-    private AuthorsRepository authorsRepository;
+    private AuthorsService authorsService;
 
     @GetMapping
     public List<Author> getAllAuthors() {
-        return authorsRepository.findAll();
+        return authorsService.getAllAuthors();
     }
 
     @PostMapping
     public Author createAuthor(@RequestBody Author author) {
-        return authorsRepository.save(author);
+        return authorsService.createAuthor(author);
     }
 
     @GetMapping("/{id}")
     public Author getAuthorsById(@PathVariable UUID id) {
-        return authorsRepository.findById(id).orElse(null);
+        return authorsService.getAuthorById(id);
     }
 
     @PutMapping("/{id}")
     public Author updateAuthor(@PathVariable UUID id, @RequestBody Author authorDetails) {
-        return authorsRepository.findById(id).map(author -> {
-            author.setName(authorDetails.getName());
-            author.setBiography(authorDetails.getBiography());
-            return authorsRepository.save(author);
-        }).orElse(null);
+        return authorsService.updateAuthor(id, authorDetails);
     }
 
     @DeleteMapping("/{id}")
     public void deleteAuthor(@PathVariable UUID id) {
-        authorsRepository.deleteById(id);
+        authorsService.deleteAuthor(id);
     }
 
     @GetMapping("/{authorId}/books")
-    public List<Book> getBooksByAuthos(@PathVariable UUID authorId) {
-        return (List<Book>) authorsRepository.findById(authorId).map(Author::getBooks).orElse(null);
+    public Set<Book> getBooksByAuthos(@PathVariable UUID authorId) {
+        return authorsService.getBooksByAuthor(authorId);
     }
 }
